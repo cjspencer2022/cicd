5124802073002cdf3d6d8ab97e5288c9e5f989d1
cjspencer2022 <cjspencer2022@gmail.com>
2023-04-27 12:25:32 -0700

    Decimal Update

--- src/main.py
+++ src/main.py
@@ -8,7 +8,8 @@ from random import randint
 
 def num_str(top: str) -> str:
     """Return a string with a die roll."""
-    if top.isnumeric():
-        return f"Your number is {randint(1, int(top))}."
-    else:
+    try:
+        top = int(top)
+        return f"Your number is {randint(1, top)}."
+    except ValueError:
         return "Please enter a number."
